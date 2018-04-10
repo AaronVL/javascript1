dfd3e36c32847eacb847ec6d7b47269e017af353
aaron lucero <avlucero1121@gmail.com>
2018-04-09 20:59:48 -0700

    worked on vars and alerts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var myHistory = "I have a general interest in technology but I like to turn this into a career if I could.";
+//console.log(myHistory);
+var myValue = confirm(myHistory);
+console.log(myValue);
