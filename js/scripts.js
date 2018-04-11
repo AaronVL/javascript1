cb70209b5e13127393e4dca34d780a6aa30c9627
aaron lucero <avlucero1121@gmail.com>
2018-04-11 15:41:36 -0700

    day 1 take home

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var myHistory = "I have a general interest in technology but I like to turn this into a career if I could.";
+var myHistory = "I have a general interest in technology but I like to turn this into a career if I could. My hobby, I don't know, watching internet videos?";
 //console.log(myHistory);
 var myValue = confirm(myHistory);
 console.log(myValue);
