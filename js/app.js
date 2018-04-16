132e8f1b91b40585ef31a0e9755cbd71c2cc4064
aaron lucero <avlucero1121@gmail.com>
2018-04-16 15:08:38 -0700

    started takehome 2

---
+++ js/app.js
@@ -0,0 +1,4 @@
+let firstName = prompt('What is your first name?');
+console.log(firstName);
+let lastName = prompt('Hello '+firstName+', what is your last name?');
+alert(lastName+", that's an interesting last name.");
