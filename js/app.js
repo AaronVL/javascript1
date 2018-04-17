adeda6bda4eb8e6b3f6037ad20aa040e8230106e
aaron lucero <avlucero1121@gmail.com>
2018-04-16 18:13:34 -0700

    added rough birthday script

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ let firstName = prompt('What is your first name?');
 console.log(firstName);
 let lastName = prompt('Hello '+firstName+', what is your last name?');
 alert(lastName+", that's an interesting last name.");
+
+
+
+
+/*
+var birthdayObject = new Date(birthday);
+var checkBirthday = confirm('So your birthday is'+ birthdayObject + '?');
+*/
