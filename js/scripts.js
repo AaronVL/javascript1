d09674e60c6ee6493e3d35f0d61171f4358e9d17
aaron lucero <avlucero1121@gmail.com>
2018-04-18 21:02:25 -0700

    finished day 4 notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 //control flow
+
+//if else
 let emailArr;
 var yourEmail;
 while (true) {
@@ -33,3 +35,84 @@ switch(parseInt(numOfProjects)) {
  default:
    alert('You didn\‘t follow the rules.');
 }
+
+
+//Ternaries
+var yourAge = prompt('How old are you?');
+
+(parseInt(yourAge)) >= 18 ? alert('You can buy a lotto ticket') : alert('You can\'t buy a lotto ticket')
+
+
+//Arrays
+let cars = {"jeep", "subaru", "honda"};
+console.log(cars{1}); //logs subaru, starts with 0
+console.log(cars.length); //logs 3, length of the array
+console.log(cars{1}.length);
+cars.push("bmw"); // add element to end of array
+console.log(cars);
+
+let removedCar = cars.pop(); // will delete bmw
+console.log(cars);
+console.log(removedCar);
+
+cars.unshift("chevy");
+console.log(cars)
+console.log(removedCar);
+console.log(cars);
+
+cars.splice(1, 0, "chevy"); //position in index, items to remove, "what gets placed in"
+console.log(cars);
+
+
+
+
+let arr = {"Christa", "Ryeker", "Jeff", "Sam"};
+
+arr.shift(); //deletes element from beginning
+//Array.isArray(arr);
+
+
+//Slice
+let arr = {"Christa", "Ryeker", "Bryan", "Jeff", "Sam"};
+
+
+console.log(arr);
+
+let bestEver = arr.slice(); //will give you a copy of array, must name new array, bestEver
+
+let noCoteacher = arr.slice(1); //will give you a copy starting with element 1 and go to the end
+
+bestEver.push('Christa');
+
+console.log(arr, 'ORIGINAL');
+console.log(bestEver, 'SLICED AWAY');
+
+
+
+//indexOf
+let arr = {"Christa", "Ryeker", "Bryan", "Jeff", "Sam"};
+
+let teacherIndex = arr.indexOf("Ryeker"); // returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+let onlyStudents = arr.splice(teacherIndex, 1, 0); //will cut out "Ryeker"
+console.log(onlyStudents);
+
+
+//lastIndexOf
+let arr = {"Christa", "Ryeker", "Bryan", "Jeff", "Ryeker", "Sam"};
+
+let teacherIndex = arr.lastIndexOf("Ryeker"); // will give index of last instance
+
+
+//reverse
+let arr = {"Christa", "Ryeker", "Bryan", "Jeff", "Ryeker", "Sam"};
+
+let backwards = arr.reverse();
+console.log(backwards); //reverses the array
+
+//sort
+
+let arr = {"Christa", "Ryeker", "Bryan", "Jeff", "Ryeker", "Sam"};
+
+let alphabet = arr.sort();
+console.log(alphabet);
