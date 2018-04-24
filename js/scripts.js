1352e8cda36d002b2544976906ae14f2c7d805da
aaron lucero <avlucero1121@gmail.com>
2018-04-23 21:00:00 -0700

    started day 5 notes

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Functions
+
+// console
+//   .log( //This is where the function is called
+//     "hello world" // This is the argument/parameter
+//   ); //Method which is a type of function
+//   //In your browser's console, the RETURN is what gets printed
+
+function sayHello(name, greeting) { //Function Definition
+  return `Hello ${name}, ${greeting}!`;
+}
+
+let myGreeting = sayHello("Connie", "You're the greatest!"); //Call Site
+
+alert(myGreeting);
+
+
+
+
+
+
+var fav = "Cheese";
+
+function sayMyFav() {
+  alert(fav);
+}
+
+sayMyFav()
