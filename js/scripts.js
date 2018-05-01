e992be766ce30598eae2411369d9082b99464517
aaron lucero <avlucero1121@gmail.com>
2018-04-30 19:09:50 -0700

    day 6 notes

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let blueDivs = document.getElementsByClassName('blue'); //Returns an array of HTML Elements with the class blue
+let h1 = document.getElementsByTagName('h1'); // Returns an array of HTML Elements that are of the tag name h1
+let para = document.getElementById('para'); // Returns the FIRST instance of an element that has the id para
+console.log(para);
