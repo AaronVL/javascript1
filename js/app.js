371859fc99b9131b584a6a55bfc175d7f55b7f81
aaron lucero <avlucero1121@gmail.com>
2018-04-30 18:45:23 -0700

    went over take home

--- js/app.js
+++ js/app.js
@@ -5,10 +5,46 @@ function squareNumber(number) {
 function askUser() {
   let numberGiven = parseInt(prompt(`Enter any number.`));
   if(!isNaN(numberGiven)) {
-    alert(squareNumber(numberGiven))
+    return alert(squareNumber(numberGiven))
   } else {
+    alert(`Not a number.`);
     askUser();
   }
 }
 
 askUser();
+
+/*
+
+
+
+*/
+
+
+
+function alertString(str) {
+  if(str.charAt(str.length - 1) !== '.') {
+    str += '.';
+  }
+  str = str.charAt(0).toUpperCase() + str.slice(1);
+  alert(str);
+}
+
+
+
+
+
+
+
+
+//BONUS
+
+function definePal(string) {
+  let reversed =
+  string.split('').reverse().join('');
+}
+
+
+
+//
+//
